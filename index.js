aed650bc4e639049b05987e7d7e39a6111097b31
eduard villamil <villamilandrey02@gmail.com>
2024-07-13 23:56:06 -0500

    Se cambio puerto de escucha del servidor

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import app from "./src/app.js";
 import { connectionDB } from "./src/db/index.js";
 
+const port = process.env.PORT || 9876
 connectionDB()
 
-app.listen(9876 , () => {
+app.listen(port , () => {
   console.log("Server is running on port 9876");
 });
