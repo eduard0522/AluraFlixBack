a67a6bd89d023c85e7893745559338f8e9a96eb0
eduard villamil <villamilandrey02@gmail.com>
2024-07-14 09:42:59 -0500

    Cambios en vercel.json para subir a producción

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import app from "./src/app.js";
 import { connectionDB } from "./src/db/index.js";
+
 import { config } from "dotenv";
+
 config()
+
+
 const port = process.env.PORT || 9876
+
 connectionDB()
 
 app.listen(port , () => {
+  
   console.log("Server is running on port ", port);
 });
