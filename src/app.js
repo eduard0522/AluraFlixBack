623b47c5330a1502d81f040b8d8044ecce3a33c7
eduard villamil <villamilandrey02@gmail.com>
2024-07-14 09:54:31 -0500

    Se agrego inicialización de base de datos en archivo de arranque

--- src/app.js
+++ src/app.js
@@ -3,8 +3,14 @@ import cors from "cors"
 import morgan from 'morgan';
 import routerVideos from './router/videos.routes.js';
 import routerGroup from './router/groups.routes.js';
+import { connectionDB } from './db/index.js';
+import { config } from "dotenv";
+
+config()
 
 const app =  express();
+const port = process.env.PORT || 9876
+
 
 app.use(morgan("combined"));
 app.use(express.json());
@@ -17,4 +23,15 @@ app.use(cors({
 app.use("/groups", routerGroup)
 app.use(routerVideos);
 
+
+connectionDB()
+
+app.listen(port , () => {
+  
+  console.log("Server is running on port ", port);
+});
+
 export default app
+
+
+
