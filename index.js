7935d242ebb61a3757a7420f2b2f6021a0bfc96c
eduard villamil <villamilandrey02@gmail.com>
2024-07-14 00:18:01 -0500

    m

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import app from "./src/app.js";
 import { connectionDB } from "./src/db/index.js";
-
+import { config } from "dotenv";
+config()
 const port = process.env.PORT || 9876
 connectionDB()
 
 app.listen(port , () => {
-  console.log("Server is running on port 9876");
+  console.log("Server is running on port ", port);
 });
