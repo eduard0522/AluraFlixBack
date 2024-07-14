e20bdb3f7d7acad58015788f9fc3bc74e6dff34a
eduard villamil <villamilandrey02@gmail.com>
2024-07-14 10:18:01 -0500

    cambio en cors

--- src/app.js
+++ src/app.js
@@ -14,9 +14,7 @@ const port = process.env.PORT || 9876
 app.use(morgan("combined"));
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173',
   credentials: true,
-
 } ));
 
 app.use("/groups", routerGroup)
